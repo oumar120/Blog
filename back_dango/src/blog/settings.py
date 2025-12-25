66b16e223c5d4aa880df59884d689462e7beb96a
omb <baldeoumar093@gmail.com>
2025-12-25 18:26:41 -0500

    fix: uncomment DATABASE_URL config for Render

--- back_dango/src/blog/settings.py
+++ back_dango/src/blog/settings.py
@@ -137,13 +137,13 @@ DATABASES = {
 }
 
 # Surcharge avec DATABASE_URL pour Render (et autres PaaS)
-'''DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
     DATABASES['default'] = dj_database_url.config(
         default=DATABASE_URL,
         conn_max_age=600,
         conn_health_checks=True,
-    )'''
+    )
 
 
 # Password validation
